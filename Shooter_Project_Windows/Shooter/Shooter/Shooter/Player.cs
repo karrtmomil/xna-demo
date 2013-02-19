74a60183da4e084b3441ab5ef3fdb7c00398da37
Karl Akert <kpakert@gmail.com>
2013-02-19 10:57:42 -0600

    Adding class Player.cs
    I added the skeleton for the player class, initialize(), Update(), and Draw()

---
+++ Shooter_Project_Windows/Shooter/Shooter/Shooter/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shooter
+{
+    class Player
+    {
+        public void Initialze()
+        {
+        }
+
+        public void update()
+        {
+        }
+
+        public void Draq()
+        {
+        }
+    }
+}
