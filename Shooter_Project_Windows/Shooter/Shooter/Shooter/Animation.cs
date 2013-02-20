7e19ee7baf8f49104d5bdcd273a63fea4bd4d604
Karl Akert <kpakert@gmail.com>
2013-02-20 10:57:12 -0600

    Updated animation.cs with skeleton
    Added all of the class variables and the three default methods

--- Shooter_Project_Windows/Shooter/Shooter/Shooter/Animation.cs
+++ Shooter_Project_Windows/Shooter/Shooter/Shooter/Animation.cs
@@ -11,5 +11,64 @@ namespace Shooter
 {
     class Animation
     {
+        // The image representing the collection of images used for animation
+        Texture2D spriteStrip;
+
+        // The scale used to display the sprite strip
+        float scale;
+
+        // The time since we last updated the fram
+        int elapsedTime;
+
+        // The time we display a frame untile the next one
+        int frameTime;
+       
+        // The number of frames that the animation contains
+        int frameCount;
+
+        // The index of the current frame we are displaying
+        int currentFrame;
+
+        // The color of the frame we will be displaying
+        Color color;
+
+        // The area of the image strip we want to display
+        Rectangle sourceRect = new Rectangle();
+
+        // The area where we want to display the image strip in the game
+        Rectangle destinationRect = new Rectangle();
+
+        // Width of a given frame
+        public int FrameWidth;
+
+        // Height of a given freame
+        public int FrameHeight;
+
+        // The state of the Animation
+        public bool Active;
+
+        // Determines if the animation will keep playing or deactivate after one run
+        public bool looping;
+
+        // Position of a given frame
+        public Vector2 position;
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+
+
     }
 }
