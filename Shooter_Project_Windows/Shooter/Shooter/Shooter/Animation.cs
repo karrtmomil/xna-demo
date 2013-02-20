b1dee4d0103349e935fd8f096a25af65c1c8ae6f
Karl Akert <kpakert@gmail.com>
2013-02-20 09:37:35 -0600

    Creating Animation.cs
    Created Animation.cs and added the using statements

---
+++ Shooter_Project_Windows/Shooter/Shooter/Shooter/Animation.cs
@@ -0,0 +1,15 @@
+﻿// Animation.cs
+// Using Declarations
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+
+namespace Shooter
+{
+    class Animation
+    {
+    }
+}
