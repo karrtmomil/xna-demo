08e8bf2ddd0545bb98b7ebd02ccbc0cc33f22180
Karl Akert <kpakert@gmail.com>
2013-02-21 19:01:35 -0600

    Added ParallaxingBackground.cs
    Added the .cs file and added a skeleton for the class.

---
+++ Shooter_Project_Windows/Shooter/Shooter/Shooter/ParallaxingBackground.cs
@@ -0,0 +1,37 @@
+﻿// ParallaxingBackground.cs
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shooter
+{
+    class ParallaxingBackground
+    { 
+        // The image representing the parallaxing background
+        Texture2D texture;
+
+        // An array of positions of the parallaxing bacground
+        Vector2[] positions;
+
+        // The speed which the background is moving
+        int speed;
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+
+    }
+}
