b55843f56ddf52fcb11163f249c1b1025b588773
Karl Akert <kpakert@gmail.com>
2013-03-09 16:24:08 -0600

    Updated ParallaxingBackground.cs Initialize()
    Updated the Initialize() function to set up an background instance

--- Shooter_Project_Windows/Shooter/Shooter/Shooter/ParallaxingBackground.cs
+++ Shooter_Project_Windows/Shooter/Shooter/Shooter/ParallaxingBackground.cs
@@ -17,8 +17,24 @@ namespace Shooter
         // The speed which the background is moving
         int speed;
 
-        public void Initialize()
+        public void Initialize(ContentManager content, String texturePath, int screenWidth, int speed)
         {
+            // Load the background texture we will be using 
+            texture = content.Load<Texture2D>(texturePath);
+
+            // Set teh speed of the background
+            this.speed = speed;
+
+            // If we divide the screen with the texture widthe then we can determine the number of tiles we need.
+            // We add 1 to it so that we won't have a gap in the tiling
+            positions = new Vector2[screenWidth / texture.Width + 1];
+
+            // Set the initial positions of the parallaxing background
+            for (int i = 0; i < positions.Length; i++)
+            {
+                // We need the tiles to be side by side to create a tiling effect
+                positions[i] = new Vector2(i * texture.Width, 0);
+            }
 
         }
 
