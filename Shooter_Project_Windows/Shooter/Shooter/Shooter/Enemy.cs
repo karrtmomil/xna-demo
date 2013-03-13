88e90ae3492fc367152161827fe1ff6ca8f2dde6
Karl Akert <kpakert@gmail.com>
2013-03-12 21:56:16 -0500

    Finished updating Enemy.cs
    Fleshed Update() and Draw()

--- Shooter_Project_Windows/Shooter/Shooter/Shooter/Enemy.cs
+++ Shooter_Project_Windows/Shooter/Shooter/Shooter/Enemy.cs
@@ -64,12 +64,30 @@ namespace Shooter
             Value = 100;
         }
 
-        public void Update()
+        public void Update(GameTime gameTime)
         {
+            // The enemy always moves to the left so decrement it's xposition
+            Position.X -= enemyMoveSpeed;
+
+            // Update the position of the Animation
+            EnemyAnimation.Position = Position;
+
+            // Update Animation
+            EnemyAnimation.Update(gameTime);
+
+            // If the enemy is past the screen or its health reaches 0 then deactivateit
+            if (Position.X < -Width || Health <= 0)
+            {
+                // By setting the Active flag to false, the game will remove this objet fromthe 
+                // active game list
+                Active = false;
+            }
         }
 
-        public void Draw()
+        public void Draw(SpriteBatch spriteBatch)
         {
+            // Draw the animation
+            EnemyAnimation.Draw(spriteBatch);
         }
     }
 }
